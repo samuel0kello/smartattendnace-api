ee475b11a32735e81bd8a68a694967c8b47e95ea
okello <samuelokello929@gmail.com>
2025-07-16 18:01:48 +0300

    feat: Initial Project setup - bAsic project setup - Dependency injection setup - Health endpoint implementation

--- src/main/kotlin/com/example/plugins/DI.kt
+++ src/main/kotlin/com/example/plugins/DI.kt
@@ -1,15 +1,2 @@
 package com.example.plugins
 
-import com.example.di.appModule
-import com.example.di.configModule
-import com.example.di.securityModule
-import io.ktor.server.application.*
-import org.koin.ktor.plugin.Koin
-import org.koin.logger.slf4jLogger
-
-fun Application.configureKoin() {
-    install(Koin) {
-        slf4jLogger()
-        modules(appModule, configModule, securityModule)
-    }
-}
